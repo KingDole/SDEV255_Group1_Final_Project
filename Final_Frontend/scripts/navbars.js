a310d72223fabf600d290c00cea44ad2a5fcec6a
KingDole <brandon.bartee1989@gmail.com>
2025-10-13 16:15:08 -0400

    Logout Button
    More logout button stuff

--- Final_Frontend/scripts/navbars.js
+++ Final_Frontend/scripts/navbars.js
@@ -2,27 +2,30 @@ document.addEventListener("DOMContentLoaded", loadNavbar);
 
 function loadNavbar() {
     //Get role from localStorage
-    const role = localStorage.getItem("urole");
+    const role = localStorage.getItem("urole")
 
     //Make Student navbar the default
-    let navbarHtml = `
+    let studentNavbar = `
         <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/index.html">All Courses</a>
         <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/add_course.html">Add Course</a>
         <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/current_schedule.html">Current Schedule</a>
-    `;
+        <button id="logout" style="float: right;">Logout</button>
+    `
 
-    //Change navbar if the role is Teacher
-    if (role === "Teacher") {
-        navbarHtml += `
-            <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/index.html">All Courses</a>
-            <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/current_schedule.html">Current Schedule</a>
-            <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/create_course.html">Course Management</a>
-        `;
-    }
+    let teacherNavbar = `
+        <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/index.html">All Courses</a>
+        <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/current_schedule.html">Current Schedule</a>
+        <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/create_course.html">Create Course</a>
+        <a href="https://kingdole.github.io/SDEV255_Group1_Final_Project/Final_Frontend/edit_delete.html">Course Management</a>
+        <button id="logout" style="float: right;">Logout</button>
+    `
 
     // Inject navbar into the <nav> tag
-    const nav = document.querySelector("nav");
-    if (nav) {
-        nav.innerHTML += navbarHtml;
+    const nav = document.querySelector("nav")
+    if (role == "Teacher") {
+        nav.innerHTML = teacherNavbar
+    }
+    else {
+        nav.innerHTML = studentNavbar
     }
 }
