58c860f98aceebb479b234ff9295be219b86a179
KingDole <brandon.bartee1989@gmail.com>
2025-10-14 11:22:55 -0400

    Backend
    Backend corrections for proper routing

--- Final_Backend/models/users.js
+++ Final_Backend/models/users.js
@@ -2,10 +2,10 @@ const db = require("../db");
 
 // Create a model from the schema
 const User = db.model("User", {
-    username: {type: String, require: true},
-    password: {type: String, require: true},
-    role: {type: String, require: true},
-    schedule: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    role: {type: String, required: true},
+    schedule: [{ type: db.Schema.Types.ObjectId, ref: 'Course' }]
 })
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = User;
